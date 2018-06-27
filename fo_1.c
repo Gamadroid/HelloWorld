54608ebbd3074d0b5bae116a3a8570a422cd286e
Gamadroid <40616911+Gamadroid@users.noreply.github.com>
2018-06-27 09:29:14 +0800

    Add files via upload

---
+++ fo_1.c
@@ -0,0 +1,13 @@
+/* Name: Joshua Wee			Class: ES22
+This program uses a for-loop to print Hello 3 times
+*/
+
+#include <stdio.h>
+
+void main(void) {
+	int n;
+
+	for (n = 1; n <= 3; n = n + 1) {
+		printf("Hello\n");
+	}
+}
